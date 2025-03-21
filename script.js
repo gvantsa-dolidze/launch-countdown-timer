141f5abef8e432650b728cba2d615d0e8bfc556a
gvantsa <gvantsajikhvadze93@gmail.com>
2025-03-21 15:28:28 +0400

    Added responsive styling with flexbox for layout, centered content

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const countdown = setInterval(() => {
   const result = document.querySelector(".show-result");
   const today = new Date().getTime();
   const countdownDay = new Date("Mar 29, 2025, 18:00:00").getTime();
-
   const daysLeft = countdownDay - today;
 
   const days = String(Math.floor(daysLeft / (1000 * 60 * 60 * 24))).padStart(
@@ -22,19 +21,19 @@ const countdown = setInterval(() => {
   result.innerHTML = `
     <div class='center'>
         <p>${days}</p>
-        <p>days</p>
+        <p>DAYS</p>
       </div>
       <div class='center'>
         <p>${hours}</p>
-        <p>hours</p>
+        <p>HOURS</p>
       </div>
       <div class='center'>
         <p>${minutes}</p>
-        <p>minutes</p>
+        <p>MINUTES</p>
       </div>
       <div class='center'>
         <p>${seconds}</p>
-        <p>secondes</p>
+        <p>SECONDES</p>
       </div>
   `;
 }, 1000);
